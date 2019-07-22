8e780ea9e602b537e2012130cc9f6486f748eaa8
ananduks <52703105+ananduks@users.noreply.github.com>
2019-07-22 09:16:12 +0530

    Update and rename heart.py to malar.py
    Malar

--- malar.py
+++ malar.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.heart", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.malar", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("💙💛💓💔💘💕💜💚💝💞💟"))
+	deq = deque(list("കോളനി വാണം 🙈🤭😠😠സ്പോട്ടെഡ്🤣🤣മലരേ💩💩പോ😂🤣🤣🤣🤣")
 	for _ in range(100):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
