1ad8632da2d03738e5fede740329cdee7939571b
ananduks <52703105+ananduks@users.noreply.github.com>
2019-07-22 08:40:05 +0530

    Update and rename heart.py to pinkus.py

--- pinkus.py
+++ pinkus.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.heart", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.pinkus", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("💙💛💓💔💘💕💜💚💝💞💟"))
+	deq = deque(list("💙💛💓💕love💜u💚💝pinkus💞💟"))
 	for _ in range(100):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
