b4bb930e697a22b4059745d51686c7b0a77ada15
ananduks <52703105+ananduks@users.noreply.github.com>
2019-08-12 11:15:44 +0530

    Update and rename sruthy.py to kanji.py

--- kanji.py
+++ kanji.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.sruthy", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.kanji", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("പൊട്ടി ശ്രുതി മണ്ടി ശ്രുതി അഡ്മിനെ കണ്ടാൽ ചൊറിയും ശ്രുതി😂😂🤣🤣😭😭"))
+	deq = deque(list("പൊട്ടി കുഞ്ഞി മണ്ടി കുഞ്ഞി കഞ്ഞി കുഞ്ഞി മാഷാ കുഞ്ഞി😂😂🤣🤣😭😭"))
 	for _ in range(100):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
