f9ce9b93565c42ec0eccd9b3c6a81e10db904e12
ananduks <52703105+ananduks@users.noreply.github.com>
2019-08-12 11:01:14 +0530

    Update and rename Monus.py to sruthy.py

--- sruthy.py
+++ sruthy.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.monus", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.sruthy", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("അട മോനെ ഇതൊന്നും അത്ര നല്ലതല്ല കെട്ടോ🤣🤣🤣"))
+	deq = deque(list("പൊട്ടി ശ്രുതി മണ്ടി ശ്രുതി അഡ്മിനെ കണ്ടാൽ ചൊറിയും ശ്രുതി😂😂🤣🤣😭😭"))
 	for _ in range(100):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
